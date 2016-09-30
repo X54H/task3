34b1e97ed744509e089de67f4e4375465c156136
X54H <giuseppe.online92@gmail.com>
2016-09-30 14:42:34 +0300

    Initiatl Commit

---
+++ src/main/java/com/jyu/task3/api/MovieApi.java
@@ -0,0 +1,44 @@
+package com.jyu.task3.api;
+
+import com.jyu.task3.user.User;
+import com.jyu.task3.user.Users;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+
+@Path("/movies")
+public class MovieApi {
+
+  //return movies list
+  @GET
+  @Produces({"application/json", "application/xml"})
+  public Response get(){
+
+    return Response.ok(Users.getInstance()).build();
+
+  }
+
+  //you can inser an user
+  @Path("add")
+  @POST
+  @Consumes({"application/json", "application/xml"})
+  @Produces({"application/json", "application/xml"})
+  public Response addUser(User u){
+    Users.getInstance().add(u);
+    return Response.ok().build();
+  }
+
+  //return an user
+  @Path("get/{name}")
+  @GET
+  @Produces({"application/json", "application/xml"})
+  public Response getByName(@PathParam("name") String name){
+    User u = Users.getInstance().getByName(name);
+    if(u!=null)
+      return Response.ok(u).build();
+    else
+      return Response.status(Response.Status.NOT_FOUND).build();
+  }
+} 
